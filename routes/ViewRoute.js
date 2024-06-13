04a172ac1e4c3c76853cc1e327cca7bda27f36b2
Michael <michicko81@gmail.com>
2024-06-13 04:45:18 +0100

    setup some pages

--- routes/ViewRoute.js
+++ routes/ViewRoute.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { getHome } = require('../controller/ViewController');
+const { getHome, getAbout, getAdmission, getLifeAtKellex, getGallery, getNewsAndEvents, getLogin, getAcademics } = require('../controller/ViewController');
 const router = express.Router();
 
 
 router.get('/', getHome);
+router.get('/about-us', getAbout)
+router.get('/admission', getAdmission)
+router.get('/academics', getAcademics)
+router.get('/life-at-kellex', getLifeAtKellex)
+router.get('/gallery', getGallery)
+router.get('/news-and-events', getNewsAndEvents)
+router.get('/login', getLogin)
 
 
 module.exports = router;
